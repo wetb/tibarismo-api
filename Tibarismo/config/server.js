85e4b053c23006a51227016e7028619133029df5
wetb <walterbenavides03@gmail.com>
2021-04-13 12:40:57 -0500

    En esta parte he creado la estructura del backend utilizando strapi y conectado a un base de datos mysql por el puero 3306 y usuario root. Los siguientes comando se deben ingresar en la terminal del equipo (cmd) o la terminal del editor. "npm run develop"--> para ejeciutar un servidor de desarrollo "npm run start"--> para desplegarlo "npm build"--> paar convertir la app a producción "npm run strapi"--> para ver los comandos de strapi.
    para iniciar la construcción se debe ingresar la siguiente ruta en el
    navegador http://localhost:1337/admin/auth/register-admin.

---
+++ Tibarismo/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd68b39ddf033cfe0d8dd9d948970565e'),
+    },
+  },
+});
---
+++ Tibarismo/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e0381161-a05c-4c64-a8ae-dd2f72764268'
+};
